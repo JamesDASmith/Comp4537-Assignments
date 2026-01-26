ae62ae31f8a527687d6823e6a0ab0850fd0d1900
JamesDASmith <James.DASmith649@gmail.com>
2026-01-25 16:24:38 -0800

    lab 2 structure changes

---
+++ lab2/lang/messages/en/user.js
@@ -0,0 +1,19 @@
+export const MESSAGES = {
+    APP_TITLE:"Lab 2",
+    AUTHOR_NAME:"James Smith",
+
+    ADD_NOTE: "Add Note",
+    REMOVE_NOTE: "Remove",
+    BACK: "Back",
+
+    HOME: "Home",
+    READER: "Reader",
+    WRITER: "Writer",
+
+    PROMPT: "Would you like to read or write?",
+
+    SAVED_AT: "Last saved at: ",
+    LOADED_AT: "Last loaded at: ",
+
+    NO_NOTES: "No notes available.",
+};
